e2d81cd8b83eeafaee5638fee8da853f4654c1d6
babospal <babos.pal@students.jedlik.eu>
2026-03-18 09:03:36 +0100

    coin sound added

--- Coin.py
+++ Coin.py
@@ -7,6 +7,7 @@ class Coin(pygame.sprite.Sprite):
     _image: pygame.Surface
     _rect: pygame.Rect
     _speed: float
+    _coin_sound: pygame.mixer.Sound
     def __init__(self, speed: float) -> None:
         super().__init__()
         self._image = pygame.Surface((20, 20), pygame.SRCALPHA)
@@ -14,6 +15,7 @@ class Coin(pygame.sprite.Sprite):
         self._rect = self._image.get_rect()
         self._rect.x = WIDTH
         self._rect.y = random.randint(240, 320)
+        self._coin_sound = pygame.mixer.Sound("coin sound.wav")
 
         self._speed = speed
 
@@ -22,4 +24,5 @@ class Coin(pygame.sprite.Sprite):
 
         if self._rect.right < 0:
             self.kill()
-
+            pygame.mixer.Sound.play(self._coin_sound)
+            pygame.mixer.music.stop()
